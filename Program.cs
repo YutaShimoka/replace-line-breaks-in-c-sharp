e47a31c42db010bb708d603e059074950e04f69a
YutaShimoka <118116907+YutaShimoka@users.noreply.github.com>
2022-11-24 07:07:36 +0900

    Change to PascalCase

--- Program.cs
+++ Program.cs
@@ -10,24 +10,24 @@ namespace replace_line_breaks_in_c_sharp
 {
     internal class Program
     {
-        private static readonly string PATTERN = ConfigurationManager.AppSettings["PATTERN"];
-        private static readonly string REPLACEMENT = ConfigurationManager.AppSettings["REPLACEMENT"];
+        private static readonly string Pattern = ConfigurationManager.AppSettings["Pattern"];
+        private static readonly string Replacement = ConfigurationManager.AppSettings["Replacement"];
 
         static void Main(string[] args)
         {
             string input = "a\r\nb\rc\nd";
             System.Diagnostics.Debug.WriteLine("[debug] input: " + input);
 
-            System.Diagnostics.Debug.WriteLine("[debug] pattern: " + PATTERN);
-            System.Diagnostics.Debug.WriteLine("[debug] replacement: " + REPLACEMENT);
+            System.Diagnostics.Debug.WriteLine("[debug] pattern: " + Pattern);
+            System.Diagnostics.Debug.WriteLine("[debug] replacement: " + Replacement);
 
-            string output = Regex.Replace(input, PATTERN, REPLACEMENT);
+            string output = Regex.Replace(input, Pattern, Replacement);
             System.Diagnostics.Debug.WriteLine("[debug] output: " + output);
 
-            System.Diagnostics.Debug.WriteLine("[debug] result: " + assertEquals("a<br>b<br>c<br>d", output));
+            System.Diagnostics.Debug.WriteLine("[debug] result: " + AssertEquals("a<br>b<br>c<br>d", output));
         }
 
-        private static string assertEquals(string expacted, string actual)
+        private static string AssertEquals(string expacted, string actual)
         {
             return expacted.Equals(actual) ? "ok" : "ng";
         }
